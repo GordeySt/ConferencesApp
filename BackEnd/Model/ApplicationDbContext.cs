6dd2ae0f7bc004c7e1ca5067f737f9fef55e3b0c
gordeyst <gordeystahovets@gmail.com>
2021-01-17 15:36:25 +0300

    Add OnModelCreating method

--- BackEnd/Model/ApplicationDbContext.cs
+++ BackEnd/Model/ApplicationDbContext.cs
@@ -10,6 +10,21 @@ namespace BackEnd.Model
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendee>()
+            .HasIndex(a => a.UserName)
+            .IsUnique();
+
+            // Many-to-many: Session <-> Attendee
+            modelBuilder.Entity<SessionAttendee>()
+                .HasKey(ca => new { ca.SessionId, ca.AttendeeId });
+
+            // Many-to-many: Speaker <-> Session
+            modelBuilder.Entity<SessionSpeaker>()
+                .HasKey(ss => new { ss.SessionId, ss.SpeakerId });
+        }
+
         public DbSet<Session> Sessions { get; set; }
 
         public DbSet<Track> Tracks { get; set; }
