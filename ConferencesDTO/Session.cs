ef0ca88d3dd5cfa16b59afa001a1facefe88c2a0
gordeyst <gordeystahovets@gmail.com>
2021-01-17 15:11:50 +0300

    Fix session file

--- ConferencesDTO/Session.cs
+++ ConferencesDTO/Session.cs
@@ -25,31 +25,3 @@ namespace ConferencesDTO
         public virtual string EmailAddress { get; set; }
     }
 }
-Create a Session.cs class with the following code:
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-namespace ConferenceDTO
-{
-    public class Session
-    {
-        public int Id { get; set; }
-
-        [Required]
-        [StringLength(200)]
-        public string Title { get; set; }
-
-        [StringLength(4000)]
-        public virtual string Abstract { get; set; }
-
-        public virtual DateTimeOffset? StartTime { get; set; }
-
-        public virtual DateTimeOffset? EndTime { get; set; }
-
-        public TimeSpan Duration => EndTime?.Subtract(StartTime ?? EndTime ?? DateTimeOffset.MinValue) ?? TimeSpan.Zero;
-
-        public int? TrackId { get; set; }
-    }
-}
